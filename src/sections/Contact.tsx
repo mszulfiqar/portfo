91c9e2f5d4d82d51c6e1b93b9d746910e4496c21
mszulfiqar <mszulfiqarkhan@gmail.com>
2024-09-22 12:00:08 -0700

    ghdc

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -12,7 +12,7 @@ export const ContactSection = () => {
           <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">Let&apos;s create something amazing togther</h2>
-              <p className="text-sm md:text-base mt-2">Ready to bring your next project to life? Let's connect and discuss how can I help you achieve your goals.</p>
+              <p className="text-sm md:text-base mt-2">Ready to bring your next project to life? Let&apos;s connect and discuss how can I help you achieve your goals.</p>
             </div>
             <div>
             <button className="text-white rounded-xl bg-gray-900 h-12 px-6 font-semibold inline-flex items-center justify-center gap-2 w-max
